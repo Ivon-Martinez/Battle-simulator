ba486293ea4e8f3175ac915a5aac752b8fb32954
TB <thrishbg15@gmail.com>
2025-11-04 21:12:58 +0000

    feat: add battle logic

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,35 @@ import * as readline from 'readline';
 interface Character {
     name: string;
     hp: number;
-    attack?: number;
-    damage: number;
-    isAlive(): boolean;
     power: string;
+    attack: number;
+    isAlive(): boolean;
     attackTarget(target: Character): void;
     damageTaken(amount: number): void;
 }
 
 class Player implements Character {
-    constructor(
-        public name: string,
-        public hp: number,
-        public power: string,
-        public damage: number
-    ) {}
+    name: string;
+    hp: number;
+    power: string;
+    attack: number;
+    constructor(name:string, hp:number, power:string, attack:number) {
+        this.name = name;
+        this.hp = hp;
+        this.power = power;
+        this.attack = attack;
+    }
     isAlive(): boolean {
         return this.hp > 0;
     }
     attackTarget(target: Character): void {
-        target.damageTaken(this.damage);
+        console.log(`${this.name} attacks ${target.name}`)
+        target.damageTaken(this.attack);
     }
     damageTaken(amount: number): void {
-        this.hp -= amount;
+        const randomDamage = Math.floor(Math.random() * amount) + 1;
+        this.hp -= randomDamage;
+        console.log(`${this.name} takes ${randomDamage} damage, remaining HP: ${this.hp}`);
     }
 }
 
@@ -40,17 +46,38 @@ function askQuestion(query: string): Promise<string> {
 
 async function battle(player1: Character, player2: Character): Promise<void> {
 
-    const choice = await askQuestion('Choose your character WaterAvatar or FireAvatar (w/f): ')
+    const choice = await askQuestion('Choose your character WaterAvatar or FireAvatar (w/f): ');
+
     while (player1.isAlive() && player2.isAlive()) {
+        console.log(`\n${player1.name} HP: ${player1.hp} | ${player2.name} HP: ${player2.hp}`);
+
         if (choice.toLowerCase() == 'w') {
-            playerOne.attackTarget(playerTwo);
+            player1.attackTarget(player2);
+            if (player2.isAlive()){
+                player2.attackTarget(player1);
+            }
         } else if (choice.toLowerCase() == 'f') {
-            playerTwo.attackTarget(playerOne)
+            player2.attackTarget(player1);
+            if (player1.isAlive()) {
+                player1.attackTarget(player2)
+            }
+        } else {
+            console.log('Invalid input. Please enter "a" to attack or "r" to run.');
         }
     }
+
+    if (player1.isAlive() && !player2.isAlive()) {
+        console.log(`${player1.name} wins!`);
+    } else {
+        console.log(`${player2.name} wins!`);
+    } 
+
+    rl.close();
 }
 
-const playerOne = new Player("WaterAvatar", 100, "Freeze", 15,);
-const playerTwo = new Player("FireAvatar", 100, "Fireball", 15,);
 
-console.log(`Character: ${playerOne.name}, HP: ${playerOne.hp}, Power: ${playerOne.power}, Damage: ${playerOne.damage}, Alive: ${playerOne.isAlive}`);
+const player1 = new Player("WaterAvatar", 100, "Freeze", 15,);
+const player2 = new Player("FireAvatar", 100, "Fireball", 15,);
+
+battle(player1, player2);
+
